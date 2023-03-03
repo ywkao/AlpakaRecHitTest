4effed5337ed07ae68c47049477731c1594bc03f
ywkao <ykao@cern.ch>
2023-03-03 06:40:03 +0100

    Trials to incorporate RecHit collections

--- plugins/alpaka/TestAlgo.dev.cc
+++ plugins/alpaka/TestAlgo.dev.cc
@@ -15,25 +15,28 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
   public:
     template <typename TAcc, typename = std::enable_if_t<alpaka::isAccelerator<TAcc>>>
     ALPAKA_FN_ACC void operator()(TAcc const& acc,
-                                  portabletest::TestDeviceCollection::View view,
+                                  portabletest::TestDeviceRecHitCollection::View view,
                                   int32_t size, double xvalue) const {
       // global index of the thread within the grid
       const int32_t thread = alpaka::getIdx<alpaka::Grid,alpaka::Threads>(acc)[0u];
-      const portabletest::Matrix matrix{{1, 2, 3, 4, 5, 6}, {2, 4, 6, 8, 10, 12}, {3, 6, 9, 12, 15, 18}};
 
-      // set this only once in the whole kernel grid
-      if (thread == 0) {
-        view.r() = 1.;
-      }
-  
-      // make a strided loop over the kernel grid, coveringup to "size" elements
+      //--- placeholder for calibration algorithms ---//
+      // .
+      // .
+      // .
+      // .
+      // .
+      //--- end of calibration algorithms ---//
+
+      // make a strided loop over the kernel grid, covering up to "size" elements
       for (int32_t i : cms::alpakatools::elements_with_stride(acc, size)) {
-        view[i] = {xvalue, 0., 0., i, matrix * i};
+        // event, detid, adc, adcm, adc_cm, toa, tot, triglatency
+        view[i] = {0, 0, 3., 4., 5., 6., 7., 8.};
       }
     }
   };
 
-  void TestAlgo::fill(Queue& queue, portabletest::TestDeviceCollection& collection, double xvalue) const {
+  void TestAlgo::fill(Queue& queue, portabletest::TestDeviceRecHitCollection& collection, double xvalue) const {
     // use 64 items per group (this value is arbitrary, but it's a reasonable starting point)
     uint32_t items = 64;
 
@@ -41,8 +44,8 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     uint32_t groups = divide_up_by(collection->metadata().size(), items);
 
     // map items to
-    // -threadswithasingleelementperthreadonaGPUbackend
-    // -elementswithinasinglethreadonaCPUbackend
+    // - threads with a single element per thread on a GPU backend
+    // - elements within a single thread on a CPU backend
     auto workDiv = make_workdiv<Acc1D>(groups, items);
 
     alpaka::exec<Acc1D>(queue, workDiv, TestAlgoKernel{}, collection.view(), collection->metadata().size(), xvalue);
