9e2a4943ed5126b94504dad947e7c1fdcf5e16d9
ywkao <ykao@cern.ch>
2023-03-03 07:42:18 +0100

    Make classes present in the dictionary

---
+++ DataFormats/src/alpaka/classes_serial.h
@@ -0,0 +1,5 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Portable/interface/Product.h"
+#include "DataFormats/PortableTestObjects/interface/TestHostCollection.h"
+#include "DataFormats/PortableTestObjects/interface/TestSoA.h"
+#include "DataFormats/PortableTestObjects/interface/TestRecHitSoA.h"
---
+++ DataFormats/src/classes.h
@@ -0,0 +1,2 @@
+#include "DataFormats/PortableTestObjects/interface/TestSoA.h"
+#include "DataFormats/PortableTestObjects/interface/TestRecHitSoA.h"
