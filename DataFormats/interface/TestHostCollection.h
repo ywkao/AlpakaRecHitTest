52cfe25acd21530b04f46d22b75e280df65b451f
ywkao <ykao@cern.ch>
2023-03-03 07:22:29 +0100

    Track modified files in DataFormats

---
+++ DataFormats/interface/TestHostCollection.h
@@ -0,0 +1,16 @@
+#ifndef DataFormats_PortableTestObjects_interface_TestHostCollection_h
+#define DataFormats_PortableTestObjects_interface_TestHostCollection_h
+
+#include "DataFormats/Portable/interface/PortableHostCollection.h"
+#include "DataFormats/PortableTestObjects/interface/TestSoA.h"
+//#include "DataFormats/PortableTestObjects/interface/TestRecHitSoA.h"
+
+namespace portabletest {
+
+  // SoA with x, y, z, id fields in host memory
+  using TestHostCollection = PortableHostCollection<TestSoA>;
+  //using TestHostRecHitCollection = PortableHostCollection<TestRecHitSoA>;
+
+}  // namespace portabletest
+
+#endif  // DataFormats_PortableTestObjects_interface_TestHostCollection_h
